db7630d03494f4dddbc150da87ac1f177817874c
vpss22 <147230534+vpss22@users.noreply.github.com>
2025-10-20 06:51:39 -0700

    Create db.js

---
+++ config/db.js
@@ -0,0 +1,20 @@
+// const db = {
+//     data : { 
+//         host : 'localhost',
+//         username : 'pixelnx',
+//         password : 'qJS82ZACy7$zK',
+//         database : 'inboxgun',
+//         timezone: '+05:30' 
+//     }
+// }
+const db = {
+    data : { 
+        host : 'localhost',
+        username : 'root',
+        password : '',
+        database : 'inboxgun',
+        timezone: '+05:30' 
+    }
+}
+// /
+module.exports = db
